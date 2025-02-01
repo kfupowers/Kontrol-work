86b989940b4e053eccc355850a11f7237bf29a9e
Ramil <ramilpowers@gmail.com>
2025-02-01 11:54:22 +0300

    seccond commit

--- src/main/java/modul/Roof.java
+++ src/main/java/modul/Roof.java
@@ -1,5 +1,7 @@
 package modul;
 
+import java.util.Random;
+
 public class Roof extends Stage {
     public Roof(int cost, String start_date, String end_date) {
         super(cost, start_date, end_date);
@@ -7,11 +9,17 @@ public class Roof extends Stage {
 
     @Override
     public void next() {
-
+        Random random = new Random();
+        String start_d = String.valueOf(random.nextInt(30)+1)+"."+String.valueOf(random.nextInt(11)+1)+"."+String.valueOf(random.nextInt(40)+2000);
+        String end_d = String.valueOf(random.nextInt(30)+1)+"."+String.valueOf(random.nextInt(11)+1)+"."+String.valueOf(random.nextInt(40)+2000);
+        new Trim(random.nextInt(10000000)+1000000, start_d, end_d);
     }
 
     @Override
     public void prev() {
-
+        Random random = new Random();
+        String start_d = String.valueOf(random.nextInt(30)+1)+"."+String.valueOf(random.nextInt(11)+1)+"."+String.valueOf(random.nextInt(40)+2000);
+        String end_d = String.valueOf(random.nextInt(30)+1)+"."+String.valueOf(random.nextInt(11)+1)+"."+String.valueOf(random.nextInt(40)+2000);
+        new Walls(random.nextInt(10000000)+1000000, start_d, end_d);
     }
 }
